2457f258090499b0491cd49eca66992a792b2a47
Tanveer Sidiq <tanveer.sidiq@gmail.com>
2017-03-10 23:53:58 +0530

    Added Cloud Formation tempaltes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+process.env.DATABASE_URL = process.env.DATABASE_URL ||
+    'mysql://' + process.env.DB_USER + ':' +
+    process.env.DB_PASSWORD + '@' +
+    process.env.DB_HOST + '/' +
+    process.env.DB_NAME;
+
+process.env.JWT_SECRET = process.env.JWT_SECRET || "imlovingit";
+
 var express = require('express'),
     bodyParser = require('body-parser'),
     routes = require('./tsbb-api/routes.js');
 
 var app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(bodyParser.json());
 
@@ -12,7 +22,7 @@ app.use(express.static(__dirname + '/tsbb-web'));
 
 routes(app, express, '/tsbb-api');
 
-var port = process.env.PORT || 3030;
+var port = process.env.PORT || 80;
 
 app.listen(port, function () {
     console.log('Running on port: ' + port);
